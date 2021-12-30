211f52d9405a705831a2acbd6d0d996b6da81b6d
David Lima <dave@davidlima.dev>
2021-12-30 16:47:16 -0300

    feat: New AccelStepper version + Arduino Mega support

--- crane_tmc2130.ino
+++ crane_tmc2130.ino
@@ -1,187 +1,166 @@
-#include <TMCStepper.h>
-
-#define EN_PIN_X           2
-#define DIR_PIN_X          3
-#define STEP_PIN_X         4
-#define CS_PIN_X           5
-
-#define EN_PIN_Y           6
-#define DIR_PIN_Y          7
-#define STEP_PIN_Y         8
-#define CS_PIN_Y           9
-
-#define EN_PIN_Z           A2
-#define DIR_PIN_Z          A3
-#define STEP_PIN_Z         A4
-#define CS_PIN_Z           A5
-
-#define SW_MOSI          11 // Software Master Out Slave In (MOSI)
-#define SW_MISO          12 // Software Master In Slave Out (MISO)
-#define SW_SCK           13 // Software Slave Clock (SCK)
-#define DRIVER_ADDRESS 0b00 // TMC2209 Driver address according to MS1 and MS2
-
-#define R_SENSE 0.11f
-
-constexpr uint32_t steps_per_mm = 4000;
-
-TMC2130Stepper driverX(CS_PIN_X, R_SENSE);                           // Hardware SPI
-TMC2130Stepper driverY(CS_PIN_Y, R_SENSE);                           // Hardware SPI
-TMC2130Stepper driverZ(CS_PIN_Z, R_SENSE);                           // Hardware SPI
-
-#include <AccelStepper.h>
-AccelStepper motorX = AccelStepper(motorX.DRIVER, STEP_PIN_X, DIR_PIN_X);
-AccelStepper motorY = AccelStepper(motorY.DRIVER, STEP_PIN_Y, DIR_PIN_Y);
-AccelStepper motorZ = AccelStepper(motorZ.DRIVER, STEP_PIN_Z, DIR_PIN_Z);
-
-const int joystickX = A0;
-const int joystickY = A1;
-int joystickXState = 0;
-int joystickYState = 0;
-
-const int buttonPin = 10;
-int buttonState = 0;
-
 #include <Servo.h>
-const int cranePin = 1;
+#include <TMCStepper.h>
+#include <AccelStepper.h>
+
+
+const int upButton = 22;
+const int rightButton = 23;
+const int downButton = 24;
+const int leftButton = 25;
+const int actionButton = 26;
+
+const int cranePin = 10;
+int cranePosition = 0;
 Servo crane;
 
-int cranePosition = 0;
+const int enPinX = 30;
+const int dirPinX = 31;
+const int stepPinX = 32;
+const int csPinX = 33;
+
+const int enPinY = 34;
+const int dirPinY = 35;
+const int stepPinY = 36;
+const int csPinY = 37;
+
+const int enPinZ = 38;
+const int dirPinZ = 39;
+const int stepPinZ = 40;
+const int csPinZ = 41;
+
+#define SW_MOSI 42
+#define SW_MISO 43
+#define SW_SCK 44
+#define R_SENSE 0.11f
+constexpr uint32_t stepsPerMm = 4000;
+
+TMC2130Stepper driverX(csPinX, R_SENSE, SW_MOSI, SW_MISO, SW_SCK);
+TMC2130Stepper driverY(csPinY, R_SENSE, SW_MOSI, SW_MISO, SW_SCK);
+TMC2130Stepper driverZ(csPinZ, R_SENSE, SW_MOSI, SW_MISO, SW_SCK);
+
+AccelStepper motorX = AccelStepper(motorX.DRIVER, stepPinX, dirPinX);
+AccelStepper motorY = AccelStepper(motorY.DRIVER, stepPinY, dirPinY);
+AccelStepper motorZ = AccelStepper(motorZ.DRIVER, stepPinZ, dirPinZ);
+
+
+boolean isRunning = false;
 
 void setup() {
-  pinMode(EN_PIN_X, OUTPUT);
-  pinMode(STEP_PIN_X, OUTPUT);
-  pinMode(DIR_PIN_X, OUTPUT);
-  digitalWrite(EN_PIN_X, LOW);
+  pinMode(upButton, INPUT);
+  pinMode(rightButton, INPUT);
+  pinMode(downButton, INPUT);
+  pinMode(leftButton, INPUT);
+  pinMode(actionButton, INPUT);
+  digitalWrite(upButton, HIGH);
+  digitalWrite(rightButton, HIGH);
+  digitalWrite(downButton, HIGH);
+  digitalWrite(leftButton, HIGH);
+  digitalWrite(actionButton, HIGH);
 
-  pinMode(EN_PIN_Y, OUTPUT);
-  pinMode(STEP_PIN_Y, OUTPUT);
-  pinMode(DIR_PIN_Y, OUTPUT);
-  digitalWrite(EN_PIN_Y, LOW);
 
-  pinMode(EN_PIN_Z, OUTPUT);
-  pinMode(STEP_PIN_Z, OUTPUT);
-  pinMode(DIR_PIN_Z, OUTPUT);
-  digitalWrite(EN_PIN_Z, LOW);
+  pinMode(enPinX, OUTPUT);
+  pinMode(stepPinX, OUTPUT);
+  pinMode(dirPinX, OUTPUT);
+  digitalWrite(enPinX, LOW);
 
-  SPI.begin();
-  driverX.begin();
-  //driverX.toff(5);
-  driverX.rms_current(600);
-  driverX.microsteps(4);
-  driverX.en_pwm_mode(true);
-  driverX.pwm_autoscale(true);
-
-  driverY.begin();
-  //driverY.toff(5);
-  driverY.rms_current(600);
-  driverY.microsteps(4);
-  driverY.en_pwm_mode(true);
-  driverY.pwm_autoscale(true);
-
-  driverZ.begin();
-  //driverZ.toff(5);
-  driverZ.rms_current(600);
-  driverZ.microsteps(4);
-  driverZ.en_pwm_mode(true);
-  driverZ.pwm_autoscale(true);
-
-  motorX.setMaxSpeed(50 * steps_per_mm); // 100mm/s @ 80 steps/mm
-  motorX.setAcceleration(1000 * steps_per_mm); // 2000mm/s^2
-  motorX.setEnablePin(EN_PIN_X);
-  motorX.setPinsInverted(false, false, true);
-  motorX.enableOutputs();
-
-  motorY.setMaxSpeed(50 * steps_per_mm); // 100mm/s @ 80 steps/mm
-  motorY.setAcceleration(1000 * steps_per_mm); // 2000mm/s^2
-  motorY.setEnablePin(EN_PIN_Y);
-  motorY.setPinsInverted(false, false, true);
-  motorY.enableOutputs();
-
-  motorZ.setMaxSpeed(50 * steps_per_mm); // 100mm/s @ 80 steps/mm
-  motorZ.setAcceleration(1000 * steps_per_mm); // 2000mm/s^2
-  motorZ.setEnablePin(EN_PIN_Z);
-  motorZ.setPinsInverted(false, false, true);
-  motorZ.enableOutputs();
-
+  setupMotors();
   crane.attach(cranePin);
-
   craneOpen();
 }
 
 void loop() {
-  joystickXState = analogRead(joystickX);
-  joystickYState = analogRead(joystickY);
-
-  int currentPositionX = motorX.currentPosition();
-  int currentPositionY = motorY.currentPosition();
-  if (joystickXState > 1000 && currentPositionX < 5000) {
+  if (digitalRead(rightButton) == LOW) {
     motorX.move(100);
   }
-  
-  if (joystickXState < 50 && currentPositionX > 0) {
+
+  if (digitalRead(leftButton) == LOW) {
     motorX.move(-100);
   }
 
-  if (joystickYState > 1000 && currentPositionY < 5000) {
+  if (digitalRead(upButton) == LOW) {
     motorY.move(100);
   }
 
-  if (joystickYState < 50 && currentPositionY > 0) {
-    motorY.move(-100);
+  if (digitalRead(downButton) == LOW) {
+    motorY.move(-100);  
   }
+  
 
   motorX.run();
   motorY.run();
   motorZ.run();
 
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) {
-     craneAction();
+  if (digitalRead(actionButton) == LOW) {
+    craneAction();
   }
 }
 
-
-
 void craneClose()
 {
-  int current = crane.read();
-  for(cranePosition = current; cranePosition < 60; cranePosition++) {
-    crane.write(cranePosition);
-    delay(5);
-  }
+  crane.write(56);
 }
 
 void craneOpen()
 {
-  int current = crane.read();
-  for(cranePosition = current; cranePosition > 0; cranePosition--) {
-    crane.write(cranePosition);
-    delay(5);
-  }
+  crane.write(0);
 }
-
 
 void craneAction()
 {
   motorX.stop();
   motorY.stop();
-  motorZ.run();
-  for (int position = 0; position <= 3000; position++) {
-    motorZ.move(100);
-  }
-  delay(4); // close crane here
+
+  motorZ.runToNewPosition(10000);
+  delay(1000); // close crane here
   craneClose();
-  for (int position = 3000; position > 0; position--) {
-    motorZ.move(-100);
-  }
-  delay(4);
+  delay(2000);
+  motorZ.runToNewPosition(0);
   motorX.run();
   motorY.run();
-  motorZ.stop();
+  delay(1000);
 
   // bring X and Y to initial position
-  motorX.move(0);
-  motorY.move(0);
-  
+  motorX.runToNewPosition(0);
+  motorY.runToNewPosition(0);
+
+  delay(1000);
   craneOpen();
+}
+
+
+void setupMotors()
+{
+  SPI.begin();
+  driverX.begin();
+  driverX.rms_current(600);
+  driverX.microsteps(4);
+  driverX.en_pwm_mode(true);
+  driverX.pwm_autoscale(true);
+  motorX.setMaxSpeed(50 * stepsPerMm);
+  motorX.setAcceleration(1000 * stepsPerMm);
+  motorX.setEnablePin(enPinX);
+  motorX.setPinsInverted(false, false, true);
+  motorX.enableOutputs();
+
+  driverY.begin();
+  driverY.rms_current(600);
+  driverY.microsteps(4);
+  driverY.en_pwm_mode(true);
+  driverY.pwm_autoscale(true);
+  motorY.setMaxSpeed(50 * stepsPerMm);
+  motorY.setAcceleration(1000 * stepsPerMm);
+  motorY.setEnablePin(enPinY);
+  motorY.setPinsInverted(false, false, true);
+  motorY.enableOutputs();
+
+  driverZ.begin();
+  driverZ.rms_current(600);
+  driverZ.microsteps(4);
+  driverZ.en_pwm_mode(true);
+  driverZ.pwm_autoscale(true);
+  motorZ.setMaxSpeed(50 * stepsPerMm);
+  motorZ.setAcceleration(1000 * stepsPerMm);
+  motorZ.setEnablePin(enPinZ);
+  motorZ.setPinsInverted(false, false, true);
+  motorZ.enableOutputs();
 }
